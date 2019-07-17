42506dc9f91c61dc1b63888ff9741ed321d4f3a0
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-16 21:12:47 -0600

    Create instance variables for all "keys" inside the CSV::Row dataset
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-authored-by: Daniel Moran <49169813+danmoran-pro@users.noreply.github.com>
    Co-authored-by: Tylor Schafer <3358870+tylorschafer@users.noreply.github.com>

--- test/game_test.rb
+++ test/game_test.rb
@@ -17,9 +17,8 @@ _start"=>"left", "venue"=>"TD Garden", "venue_link"=>"/api/v1/venues/null", "ven
   end
 
   def test_attributes
-    game_2 = Game.new({"game_id"=>"2012030221", "season"=>"20122013", "type"=>"P", "date_time"=>"2013-05-16", "away_team_id"=>"3", "home_team_id"=>"6", "away_goals"=>"2", "home_goals"=>"3", "outcome"=>"home win OT", "home_rink_side
-_start"=>"left", "venue"=>"TD Garden", "venue_link"=>"/api/v1/venues/null", "venue_time_zone_id"=>"America/New_York", "venue_time_zone_offset"=>"-4", "venue_time_zone_tz"=>"EDT"})
-    assert_equal "2012030221", game_2.game_id
+    assert_equal "2012030221", @game.game_id
+    assert_equal "2", @game.away_goals 
   end
 
 end

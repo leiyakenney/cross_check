52750850f155b2ba30ac257a5bb8fe5b17cb195f
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-16 21:16:42 -0600

    Update game_stats file to take out arguments passed
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-authored-by: Daniel Moran <49169813+danmoran-pro@users.noreply.github.com>
    Co-authored-by: Tylor Schafer <3358870+tylorschafer@users.noreply.github.com>

--- modules/game_stats.rb
+++ modules/game_stats.rb
@@ -25,10 +25,11 @@ module GameStats
     ((game_wins.count / game_collection.count.to_f) * 100).round(2)
   end
 
-  def count_of_games_by_season(season_id)
-    season_hash = {}
-    season_games = @games.find_all {|game| game.season == season_id}
-    season_hash[season_id] = season_games.count
+  def count_of_games_by_season
+    season_hash = Hash.new(0)
+    @games.each do |game|
+      season_hash[game.season] += 1
+    end
     season_hash
   end
 
@@ -37,11 +38,15 @@ module GameStats
     (total_goals / @games.count).round(2)
   end
 
-  def average_goals_by_season(season_id)
-    season_hash = {}
-    season_games = @games.find_all {|game| game.season == season_id}
-    total_goals = season_games.sum {|game| game.home_goals.to_f + game.away_goals.to_f}
-    season_hash[season_id] = (total_goals / @games.count).round(2)
-    season_hash
+  def average_goals_by_season
+    season_hash = Hash.new(0)
+    @games.each do |game|
+      season_hash[game.season] += (game.home_goals.to_f + game.away_goals.to_f)
+    end
+    avg_hash = {}
+    count_of_games_by_season.map do |game_season, sum_of_games|
+      avg_hash[game_season] = (season_hash[game_season] / sum_of_games).round(2)
+    end
+    avg_hash
   end
 end

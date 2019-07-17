7865350ea1fa011bb0b0843cf7d7e2d78194a84c
Tylor Schafer <tylorschafer@gmail.com>
2019-07-17 08:55:32 -0600

    Start adding instance vairables to teams
    Co-authored-by: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>
    Co-authored-by: Daniel Moran <49169813+danmoran-pro@users.noreply.github.com>
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- lib/team.rb
+++ lib/team.rb
@@ -1,8 +1,13 @@
 class Team
 
-  attr_reader :data
+  attr_reader :data, :team_id
 
   def initialize(data = [])
     @data = data
+    @team_id = data['team_id']
+    @franchise_id = data['franchiseid']
+    @short_name = data['shortName']
+    @team_name = data['teamName']
+
   end
 end

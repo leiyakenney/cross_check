32cfc0126258a90a40c93cf7101d565087bebda3
Tylor Schafer <tylorschafer@gmail.com>
2019-07-16 19:53:29 -0600

    Add percentage_home_wins method
    Co-authored-by: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>
    Co-authored-by: Daniel Moran <49169813+danmoran-pro@users.noreply.github.com>
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- modules/game_stats.rb
+++ modules/game_stats.rb
@@ -1,15 +1,22 @@
+require 'pry'
 module GameStats
 
   def highest_total_score
-    @games.max_by {|game| (game.data['away_goals'] + game.data['home_goals'])}
+    @games.max_by {|game| (game.data['away_goals'].to_i + game.data['home_goals'].to_i)}
   end
 
   def lowest_total_score
-    @games.min_by {|game| (game.data['away_goals'] + game.data['home_goals'])}
+    @games.min_by {|game| (game.data['away_goals'].to_i + game.data['home_goals'].to_i)}
   end
 
   def biggest_blowout
-    @games.max_by {|game| game.data['away_goals'] - game.data['home_goals']}
+    @games.max_by {|game| game.data['away_goals'].to_i - game.data['home_goals'].to_i}
+  end
+
+  def percentage_home_wins(team_id)
+    game_collection = @games.find_all {|game| game.data['home_team_id'] == team_id}
+    game_wins = game_collection.find_all {|game| game.data['outcome'].include?('win')}
+    ((game_collection.count.to_f / game_wins.count) * 100).round(2)
   end
 
 end

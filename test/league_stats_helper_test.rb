60bd121540011e6d4f5e1b190db8761f297aa4a1
Daniel <danmoran.pro@gmail.com>
2019-07-20 11:13:21 -0600

    Add tests for away_home_played and total_games_played
    Co-authored-by: Tylor Schafer <3358870+tylorschafer@users.noreply.github.com>
    Co-authored-by: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- test/league_stats_helper_test.rb
+++ test/league_stats_helper_test.rb
@@ -19,4 +19,14 @@ class LeagueStatsHelperTest < Minitest::Test
     expected = {"6"=>5, "3"=>2, "5"=>2, "16"=>4, "17"=>3, "8"=>2, "9"=>1}
     assert_equal expected, @tracker.home_games_played
   end
+
+  def test_away_games_played
+    expected = {"3"=>3, "6"=>4, "5"=>2, "17"=>4, "16"=>3, "9"=>2, "8"=>1}
+    assert_equal expected, @tracker.away_games_played
+  end
+
+  def test_total_games_played
+    expected = {"3"=>5, "6"=>9, "5"=>4, "17"=>7, "16"=>7, "9"=>3, "8"=>3}
+    assert_equal expected, @tracker.total_games_played
+  end
 end

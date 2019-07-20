3e5655d07b4914713e151bd72d78112790847307
Tylor Schafer <tylorschafer@gmail.com>
2019-07-20 11:55:32 -0600

    Add head_to_head

--- test/team_stats_module_test.rb
+++ test/team_stats_module_test.rb
@@ -26,4 +26,9 @@ class GameStatsTest < Minitest::Test
   def test_rival
     assert_equal 'Rangers', @tracker.rival("6")
   end
+
+  def test_head_to_head
+    expected = {"Rangers"=>0.8, "Penguins"=>1.0}
+    assert_equal expected, @tracker.head_to_head("6")
+  end
 end

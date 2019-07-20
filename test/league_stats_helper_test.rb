7202f76540872c2407386b25cf54883293396852
Daniel <danmoran.pro@gmail.com>
2019-07-20 12:13:23 -0600

    Add test_home_wins_percentage_by_team, test_total_away_wins_by_team, test_away_wins_percentage_by_team
    Co-authored-by: Tylor Schafer <3358870+tylorschafer@users.noreply.github.com>
    Co-authored-by: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- test/league_stats_helper_test.rb
+++ test/league_stats_helper_test.rb
@@ -79,4 +79,19 @@ class LeagueStatsHelperTest < Minitest::Test
     expected = {"6"=>5, "3"=>2, "5"=>2}
     assert_equal expected, @tracker.total_home_wins_by_team
   end
+
+  def test_home_wins_percentage_by_team
+    expected = 1.0
+    assert_equal expected, @tracker.home_wins_percentage_by_team
+  end
+
+  def test_total_away_wins_by_team
+    expected = {"3"=>3, "6"=>4, "5"=>2, "17"=>1}
+    assert_equal expected, @tracker.total_away_wins_by_team
+  end
+
+  def test_away_wins_percentage_by_team
+    expected = 1.0
+    assert_equal expected, @tracker.away_wins_percentage_by_team
+  end
 end

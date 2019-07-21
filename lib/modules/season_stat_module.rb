caf116401919503d37b5f42b890468cedbf484f4
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-20 20:08:03 -0600

    Refactor most_accurate_team

--- lib/modules/season_stat_module.rb
+++ lib/modules/season_stat_module.rb
@@ -12,10 +12,10 @@ module SeasonStat
   end
 
   def total_shots_by_season(season)
-    game_id_in_season = game_id_in_season(season)
+    # game_id_in_season = game_id_in_season(season)
     new_hash = Hash.new(0)
     @game_teams.map do |game|
-      if game_id_in_season.any? game.game_id
+      if game_id_in_season(season).any? game.game_id
         new_hash[game.team_id] += game.shots
       end
     end
@@ -23,10 +23,10 @@ module SeasonStat
   end
 
   def total_goals_by_season(season)
-    game_id_in_season = game_id_in_season(season)
+    # game_id_in_season = game_id_in_season(season)
     new_hash = Hash.new(0)
     @game_teams.map do |game|
-      if game_id_in_season.any? game.game_id
+      if game_id_in_season(season).any? game.game_id
         new_hash[game.team_id] += game.goals
       end
     end
@@ -34,19 +34,18 @@ module SeasonStat
   end
 
   def shot_ratio_by_season(season)
-    total_goals_by_season = total_goals_by_season(season)
-    total_shots_by_season = total_shots_by_season(season)
+    # total_goals_by_season = total_goals_by_season(season)
+    # total_shots_by_season = total_shots_by_season(season)
 
     new_hash = Hash.new
-    total_shots_by_season.map do |team_id, total_shots|
-      new_hash[team_id] = total_shots/total_goals_by_season[team_id].to_f
+    total_shots_by_season(season).map do |team_id, total_shots|
+      new_hash[team_id] = total_shots/total_goals_by_season(season)[team_id].to_f
     end
     new_hash
   end
 
   def minmax_shot_ratio_by_season(season)
-    shot_ratio_by_season = shot_ratio_by_season(season)
-    shot_ratio_by_season.minmax_by {|team_id, shot_ratio| shot_ratio}
+    shot_ratio_by_season(season).minmax_by {|team_id, shot_ratio| shot_ratio}
   end
 
   def most_accurate_team(season)

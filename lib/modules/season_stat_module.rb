50fdeb917d41886f828ce5a0c8da6907fdd937e7
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-20 19:35:00 -0600

    Helper methods for shot ratio by season

--- lib/modules/season_stat_module.rb
+++ lib/modules/season_stat_module.rb
@@ -5,9 +5,44 @@ module SeasonStat
 
      @games.each do |game|
        if game.season == season
-         game_id_in_season << game_id
+         game_id_in_season << game.game_id
        end
      end
   game_id_in_season
   end
+
+  def total_shots_by_season(season)
+    game_id_in_season = game_id_in_season(season)
+    new_hash = Hash.new(0)
+    @game_teams.map do |game|
+      if game_id_in_season.any? game.game_id
+        new_hash[game.team_id] += game.shots
+      end
+    end
+    new_hash
+  end
+
+  def total_goals_by_season(season)
+    game_id_in_season = game_id_in_season(season)
+    new_hash = Hash.new(0)
+    @game_teams.map do |game|
+      if game_id_in_season.any? game.game_id
+        new_hash[game.team_id] += game.goals
+      end
+    end
+    new_hash
+  end
+
+  def shot_ratio_by_season(season)
+    total_goals_by_season = total_goals_by_season(season)
+    total_shots_by_season = total_shots_by_season(season)
+
+    new_hash = Hash.new
+    total_shots_by_season.map do |team_id, total_shots|
+      new_hash[team_id] = total_shots/total_goals_by_season[team_id].to_f
+    end
+    new_hash
+  end
+
+  def minmax_shot_ratio_by_season(season)
 end
--- test/season_stats_module_test.rb
+++ test/season_stats_module_test.rb
@@ -15,9 +15,25 @@ class SeasonStatsTest < Minitest::Test
     @tracker = StatTracker.from_csv(@file_names)
   end
 
-  def test_game_id_shot_ratio_by_team
+  def test_game_id_in_season
+    skip
   #Name of the Team with the best ratio of shots to goals for the season
   #{"3"=>["2012030225", 29.0], "6"=>["2012030314", 24.0], "5"=>["2012030314", Infinity], "17"=>["2012030231", 21.0]}
   assert_equal 10000, @tracker.game_id_in_season("20122013")
+  end
+
+  def test_shot_ratio_within_season
+    skip
+  assert_equal 100000, @tracker.total_shots_by_season("20122013")
+  end
+
+  def test_total_goals_within_season
+    skip
+  assert_equal 100000, @tracker.total_goals_by_season("20122013")
+  end
+
+  def test_shot_ratio_by_season
+
+  assert_equal 10000, @tracker.shot_ratio_by_season("20122013")
   end
 end

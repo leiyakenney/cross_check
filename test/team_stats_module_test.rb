c87edac4a3fcd82c65da31bac7b04134dc338cb5
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-24 16:17:31 -0600

    Working thru merge conflicts

--- test/team_stats_module_test.rb
+++ test/team_stats_module_test.rb
@@ -80,5 +80,20 @@ class TeamStatsTest < Minitest::Test
           }
         }
     assert_equal expected, @tracker.seasonal_summary("8")
+
+    mock_data = {"20122013" =>
+                  {:regular_season => []}}
+
+    expected = {"20122013" =>
+                    {:regular_season => [],
+                    :postseason => []}}
+
+    tracker_postseason_nil = StatTracker.any_instance.stubs(:reg_vs_post).returns(mock_data)
+
+    assert_equal expected, @tracker.add_nil_post_regular_season("5")
+  end
+
+  def test_win_percentage
+
   end
 end
